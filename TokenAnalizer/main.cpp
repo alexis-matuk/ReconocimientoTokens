5715e9204e08b403c18c912b560e779c9d4165c0
Alexis Matuk <amatuk@alexiss-macbook-pro.local>
2016-01-30 18:47:48 -0600

    Initial Commit

---
+++ TokenAnalizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TokenAnalizer
+//
+//  Created by Alexis Matuk on 1/30/16.
+//  Copyright © 2016 Alexis Matuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
